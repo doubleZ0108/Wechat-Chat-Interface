8d1993b0769971e225e25bfed4a20691a10800a1
double Z <doubleZ0108@163.com>
2020-09-10 10:04:22 +0800

    :alembic:refactor to logic

--- static/js/main.js
+++ static/js/main.js
@@ -1,70 +1,100 @@
 window.onload = function(){
-    handleBubbleEffect();
+
+    var testData = {
+        leftPrefix: "[left-person]",
+        rightPrefix: "[right-person]",
+        recordGroup: [
+            {
+                time: "2020年8月11日",
+                records: [
+                    "[left-person]hello",
+                    "[right-person]world",
+                    "[left-person]你好",
+                    "[right-person]我来测试一下如果我发了很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长的一段话会是什么效果"
+                ]
+            },
+            {
+                time: "2020年8月11日",
+                records: [
+                    "[left-person]hello",
+                    "[right-person]world",
+                    "[left-person]你好",
+                    "[right-person]我来测试一下如果我发了很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长的一段话会是什么效果"
+                ]
+            }
+        ]
+    }
+
+    handleWechatRecordContent(testData);
+    handleIntersectionObserver();
 };
 
 
-function handleBubbleEffect() {
+function handleIntersectionObserver() {
     let observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
           if(entry.isIntersecting){
             entry.target.classList.add('animate')
-            // observer.unobserve(entry.target)
           } else {
             entry.target.classList.remove('animate')
           }
         })
     })
     
-    document.querySelectorAll('.info-group').forEach(left-person => {
-      observer.observe(left-person);
+    document.querySelectorAll('.info-group').forEach(info => {
+      observer.observe(info);
     });
 }
 
 function handleWechatRecordContent(data) {
-    this.state.data.map(({ node }) => {
-        var time = node.time
-        var records = node.records.childMarkdownRemark.rawMarkdownBody.split("\n");
-        records = records.map((record) => {
-            var item = {}
-            record = record.substr(2);
-            if(record.startsWith("left-person")){
-                item.who = "left-person";
-                item.what = record.substr(6);
-            } else if(record.startsWith("right-person")) {
-                item.who = "right-person";
-                item.what = record.substr(4);
-            }
-            return item
-        });
+    var leftPersonAvatar = "img/left-person-avatar.jpg";
+    var rightPersonAvatar = "img/right-person-avatar.jpg";
 
+    data.recordGroup.map((node) => {
         var recordContainer = document.getElementById("wechat-record-container");
         
         // Time
         var timeDiv = document.createElement("div");
         timeDiv.classList.add("time");
-        timeDiv.innerHTML = time;
+        timeDiv.innerHTML = node.time;
         recordContainer.appendChild(timeDiv);
 
+        // Records
+        var records = node.records;
+        records = records.map((record) => {
+            var item = {}
+            if(record.startsWith(data.leftPrefix)){
+                item.who = "left-person";
+                item.what = record.substr(data.leftPrefix.length);
+            } else if(record.startsWith(data.rightPrefix)) {
+                item.who = "right-person";
+                item.what = record.substr(data.rightPrefix.length);
+            }
+            return item;
+        });
         records.map((record) => {
             //Bubble
             var infoGroupDiv = document.createElement("div");
             infoGroupDiv.classList.add("info-group");
             infoGroupDiv.classList.add(record.who=="left-person" ? "left-person" : "right-person");
+
             var avatarImg = document.createElement("img");
             avatarImg.classList.add("avatar");
-            avatarImg.src = record.who=="left-person" ? left-personAvatar : right-personAvatar;
+            avatarImg.src = record.who=="left-person" ? leftPersonAvatar : rightPersonAvatar;
+            avatarImg.alt = record.who=="left-person" ? "left person avatar" : "right person avatar";
+
             var bubbleDiv = document.createElement("div");
             bubbleDiv.classList.add("bubble");
             bubbleDiv.innerHTML = record.what;
 
             if(record.who=="right-person"){
-                infoGroupDiv.appendChild(avatarImg);
                 infoGroupDiv.appendChild(bubbleDiv);
+                infoGroupDiv.appendChild(avatarImg);
             } else if (record.who=="left-person"){
-                infoGroupDiv.appendChild(bubbleDiv);
                 infoGroupDiv.appendChild(avatarImg);
+                infoGroupDiv.appendChild(bubbleDiv);
             }
             recordContainer.appendChild(infoGroupDiv);
         });
-    })
+    });
 }
